c4e4634024381fe4e464b5b8eefde4a142011542
dandical <a.itil64@gmail.com>
2018-05-25 16:36:16 +1000

    cross validation added again

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ from numpy import genfromtxt
 import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import cross_val_score, KFold
+
 import os
 PATH=os.getenv('HOME')+'/.kaggle/competitions/home-credit-default-risk'
 print(os.listdir(PATH))
-application_train = pd.read_csv(PATH+"/application_train.csv")
+x = pd.read_csv(PATH+"/application_train.csv")
 
-print(np.shape(application_train.values))
-y = application_train.pop('TARGET')
+print(np.shape(x.values))
+y = x.pop('TARGET')
 y.fillna(y.mean())
-x = application_train
 numerical_variables = list(x.dtypes [x.dtypes != "object"].index)
-x = x[numerical_variables]
+xNum = x[numerical_variables]
 #x["AMT_REQ_CREDIT_BUREAU_DAY"].fillna(x["AMT_REQ_CREDIT_BUREAU_DAY"].mean())
 #x["AMT_REQ_CREDIT_BUREAU_YEAR"].fillna(x["AMT_REQ_CREDIT_BUREAU_YEAR"].mean())
-x.is_copy = False
-for col in x:
-	x[col].fillna(x[col].mean(),inplace=True)
+xNum.is_copy = False
+for col in xNum:
+	xNum[col].fillna(xNum[col].mean(),inplace=True)
 
 model = RandomForestClassifier(n_estimators=100)
-#model.fit(x, y)
-
+#model.fit(xNum, y)
+cross_val_score(model, xNum, y, cv=10)
 
 #x_train = application_train.values[:,[0])
 

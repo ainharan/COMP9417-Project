f30d326709b5b2460bee80ec5f5c1fa6fcb398c8
dandical <a.itil64@gmail.com>
2018-05-25 15:38:39 +1000

    new vers

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 import pandas as pd 
 import numpy as np
+from numpy import genfromtxt
 import matplotlib
 import matplotlib.pyplot as plt
-#import seaborn as sns
-# matplotlib inline 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import train_test_split
 import os
 PATH=os.getenv('HOME')+'/.kaggle/competitions/home-credit-default-risk'
 print(os.listdir(PATH))
 application_train = pd.read_csv(PATH+"/application_train.csv")
-application_test = pd.read_csv(PATH+"/application_test.csv")
+
+print(np.shape(application_train.values))
+y = application_train.pop('TARGET')
+y.fillna(y.mean())
+x = application_train
+numerical_variables = list(x.dtypes [x.dtypes != "object"].index)
+x = x[numerical_variables]
+#x["AMT_REQ_CREDIT_BUREAU_DAY"].fillna(x["AMT_REQ_CREDIT_BUREAU_DAY"].mean())
+#x["AMT_REQ_CREDIT_BUREAU_YEAR"].fillna(x["AMT_REQ_CREDIT_BUREAU_YEAR"].mean())
+for col in x:
+	x[col].fillna(x[col].mean()
+
+model = RandomForestClassifier(n_estimators=100)
+#model.fit(x, y)
+
+
+#x_train = application_train.values[:,[0])
+
+
+#model.fit(x,y)
+#xtrain, xtest, ytrain, ytest = (application_train)
+#print(application_train['SK_ID_CURR'])
+
+
+'''application_test = pd.read_csv(PATH+"/application_test.csv")
 bureau = pd.read_csv(PATH+"/bureau.csv")
 bureau_balance = pd.read_csv(PATH+"/bureau_balance.csv")
 credit_card_balance = pd.read_csv(PATH+"/credit_card_balance.csv")
 installments_payments = pd.read_csv(PATH+"/installments_payments.csv")
 previous_application = pd.read_csv(PATH+"/previous_application.csv")
 POS_CASH_balance = pd.read_csv(PATH+"/POS_CASH_balance.csv")
+'''

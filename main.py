6d1bfe85488208c0ca640b6d84caa1172b8a1ad7
dandical <a.itil64@gmail.com>
2018-05-25 11:34:03 +1000

    loaded files

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ import matplotlib.pyplot as plt
 import os
 PATH=os.getenv('HOME')+'/.kaggle/competitions/home-credit-default-risk'
 print(os.listdir(PATH))
+application_train = pd.read_csv(PATH+"/application_train.csv")
+application_test = pd.read_csv(PATH+"/application_test.csv")
+bureau = pd.read_csv(PATH+"/bureau.csv")
+bureau_balance = pd.read_csv(PATH+"/bureau_balance.csv")
+credit_card_balance = pd.read_csv(PATH+"/credit_card_balance.csv")
+installments_payments = pd.read_csv(PATH+"/installments_payments.csv")
+previous_application = pd.read_csv(PATH+"/previous_application.csv")
+POS_CASH_balance = pd.read_csv(PATH+"/POS_CASH_balance.csv")

835d9816dd985cf09cda001b6bc67265949d306f
Ainharan Subramaniam <ainharan.subramaniam@gmail.com>
2018-05-25 11:18:44 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+#import seaborn as sns
+# matplotlib inline 
+import os
+PATH=os.getenv('HOME')+'/.kaggle/competitions/home-credit-default-risk'
+print(os.listdir(PATH))
